7acaee708b2b85b2801bd625e68cf9224005a10f
Sergey Pashaev <sergey.pashaev@gmail.com>
2022-03-16 11:15:15 +0300

    enable rust layer

--- init.el
+++ init.el
@@ -76,7 +76,7 @@
         )        ; using org-mode for presentations
        plantuml          ; diagrams for confusing people more
        python       ; beautiful is better than ugly
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        kotlin
 
